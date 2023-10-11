295d42ce8a213bb70fbff1d51ba63cb95995647b
lexxai <lexxai@users.noreply.github.com>
2023-10-11 23:53:43 +0300

    parse multiple pages

--- src/hw09/parse.py
+++ src/hw09/parse.py
@@ -9,39 +9,51 @@ from database.models import Quotes, Authors
 json_dest = Path(__file__).parent.joinpath("database").joinpath("json")
 
 
-def parse_data():
-    url = "https://quotes.toscrape.com/"
-    css_selector_quote = "div.quote  span.text"
-    css_selector_author_link = "div.quote:nth-child(1) > span:nth-child(2) > a"
-    css_selector_author_name = "div.quote  small.author"
-    css_selector_tags = "div.quote div.tags > a.tag"
+def parse_url(url: str) -> tuple[list[dict], next]:
+    result = []
+    next = None
+    if not url:
+        return result, next
     css_selector_next = "nav .next > a"
-
-    store_ = []
+    # print(url)
     html_doc = requests.get(url)
+    if html_doc.status_code != 200:
+        return result, next
+    soup = BeautifulSoup(html_doc.content, "html.parser")
+    quotes = soup.find_all("span", attrs={"class": "text"})
+    authors = soup.find_all("small", attrs={"class": "author"})
+    tags = soup("div", attrs={"class": "tags"})
+    next = soup.select_one(css_selector_next)
 
-    if html_doc.status_code == 200:
-        soup = BeautifulSoup(html_doc.content, "html.parser")
-        quotes = soup.find_all("span", attrs={"class": "text"})
-        authors = soup.find_all("small", attrs={"class": "author"})
-        tags = soup("div", attrs={"class": "tags"})
+    for quote, author, tag in zip(quotes, authors, tags):
+        q_text = quote.text.strip()
+        q_author = author.text.strip()
+        q_tags = [t.text.strip() for t in tag.find_all("a", attrs={"class": "tag"})]
+        result.append({"tags": q_tags, "author": q_author, "quote": q_text})
 
-        for quote, author, tag in zip(quotes, authors, tags):
+    if next:
+        next = next.get("href")
+    return result, next
 
-            q_text = quote.text.strip()
-            q_author = author.text.strip()
-            q_tags = [t.text.strip() for t in tag.find_all("a", attrs={"class": "tag"})]
-            store_.append(
-                {
-                    "tags": q_tags,
-                    "author": q_author,
-                    "quote": q_text
-                }
-            )
-    pprint(store_)
-            
-            
+
+def parse_data(base_url: str = "https://quotes.toscrape.com") -> list[dict]:
+    store_ = []
+    url = base_url
+    while True:
+        if not url:
+            break
+        result, next_url = parse_url(url)
+        store_.extend(result)
+        if not next:
+            break
+        if next_url:
+            url = base_url + next_url
+        else:
+            break
+    return store_
 
 
 if __name__ == "__main__":
-    parse_data()
+    data = parse_data()
+    print(len(data))
+    # pprint(data)
