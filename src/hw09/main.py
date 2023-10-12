a09ff1bc5cd945f36b283e4f75dc51f6a45f2516
lexxai <lexxai@users.noreply.github.com>
2023-10-12 15:33:19 +0300

    nested_parse_author

--- src/hw09/main.py
+++ src/hw09/main.py
@@ -31,18 +31,36 @@ class QuotesSpider(scrapy.Spider):
 
     def parse(self, response):
         for quote in response.xpath("/html//div[@class='quote']"):
-            keywords = list(map(lambda x: x.strip(), quote.xpath("div[@class='tags']/a/text()").getall()))
+            keywords = [
+                k.strip() for k in quote.xpath("div[@class='tags']/a/text()").getall()
+            ]
             author = quote.xpath("span/small/text()").get().strip()
+            author_link = quote.xpath("span/a/@href").get()
             quote = quote.xpath("span[@class='text']/text()").get().strip()
             yield QuoteItem(keywords=keywords, author=author, quote=quote)
+            yield response.follow(
+                url=self.start_urls[0] + author_link, callback=self.nested_parse_author
+            )
 
         next_link = response.xpath("//li[@class='next']/a/@href").get()
         if next_link:
             yield scrapy.Request(url=self.start_urls[0] + next_link)
 
     def nested_parse_author(self, response):
-        ...
- 
+        author = response.xpath("/html//div[@class='author-details']")
+        fullname = author.xpath('h3[@class="author-title"]/text()').get().strip()
+        born_date = author.xpath('p/span[@class="author-born-date"]/text()').get().strip()
+        born_location = (
+            author.xpath('p/span[@class="author-born-location"]/text()').get().strip()
+        )
+        description = author.xpath('div[@class="author-description"]/text()').get().strip()
+        yield AuthorItem(
+            fullname=fullname,
+            born_date=born_date,
+            born_location=born_location,
+            description=description,
+        )
+
 
 # run spider
 process = CrawlerProcess()
